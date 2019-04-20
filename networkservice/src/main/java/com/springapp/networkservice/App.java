64475d930c5c7d0811a892e9f6b043cbc708dbc5
3vikram <viku.vikram05@gmail.com>
2019-04-20 10:55:01 +0530

    Update App.java

--- networkservice/src/main/java/com/springapp/networkservice/App.java
+++ networkservice/src/main/java/com/springapp/networkservice/App.java
@@ -8,7 +8,7 @@ public class App
     public static void main( String[] args )
     {
         @SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         SimCard sim = context.getBean("anything", SimCard.class);
         sim.calling();
         sim.internet();
